e17433518f826ab9a0989573b49b89ea7f50f2be
karitsky <valery.karitsky@gmail.com>
2019-09-30 22:14:38 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let rules = new Map(bracketsConfig);
+  let tempStack = [];
+  let lastOne = '';
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str.charAt(i) == tempStack[tempStack.length - 1] 
+        && (str.charAt(i) === '|' 
+        || str.charAt(i) === '7' 
+        || str.charAt(i) === '8')) {
+      tempStack.pop();
+
+    } else if (rules.has(str.charAt(i))) {
+    tempStack.push(str.charAt(i));
+
+    } else {
+      lastOne = tempStack.pop();
+      if (str.charAt(i) !== rules.get(lastOne)) {
+        return false;
+      }
+    }
+  }
+
+  if (tempStack.length > 0) {
+    return false;
+  }
+
+  return true;
+
 }
